8fa53c96c24551945f47e51981fa3ff639313e5f
Kasper Munch <kaspermunch@birc.au.dk>
2024-10-26 09:24:11 +0200

    Added a remplate package

--- README.md
+++ README.md
@@ -4,10 +4,4 @@
 
 Description...
 
-## Reproducing this project
-
-This project runs in a Linux environment and may do so too on Mac. A conda environment for the project is created like this:
-
-    conda env create -f binder/environment.yml
-
-![example event parameter](https://github.com/munch-group/analysis-template/actions/workflows/quarto-publish.yml/badge.svg?event=push)
+![example event parameter](https://github.com/munch-group/project-template/actions/workflows/quarto-publish.yml/badge.svg?event=push)
---
+++ setup.py
@@ -0,0 +1,39 @@
+import setuptools, os, sys
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+with open(path.join(this_directory, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = f.read().split()
+
+setuptools.setup(
+    name="library_name",
+    version="0.1",
+    author="Kasper Munch",
+    author_email="kaspermunch@birc.au.dk",
+    description="Project template",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/munch-group/library_name",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+            'hello=library_name.bar:baz',
+            ]
+    },    
+
+    package_data={'library_name': [
+        'data/*.csv',
+        'data/*.txt',
+    ]},    
+    python_requires='>=3.6',
+    install_requires=requirements,
+    license_files = ('LICENSE.txt',),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],    
+    )
